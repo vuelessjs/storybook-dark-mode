73ec64398c922ad223cc94a3eaaf72c5586b40c7
Johnny Grid <ilevel.ws@gmail.com>
2025-11-03 11:39:50 +0800

    refactor: simplify example paths and scripts after directory restructure

--- example/.storybook/main.js
+++ example/.storybook/main.js
@@ -2,7 +2,7 @@ import { fileURLToPath } from "node:url";
 
 export default {
   stories: ["../src/**/*.stories.tsx"],
-  addons: [fileURLToPath(import.meta.resolve("../../../preset.js"))],
+  addons: [fileURLToPath(import.meta.resolve("../../preset.js"))],
   framework: {
     name: "@storybook/react-vite",
   },
